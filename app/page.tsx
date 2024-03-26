1cf82d50502f42d94e4b6b9c1907e0aa630822f4
jolley34 <erlandssonjoel@gmail.com>
2024-03-26 23:18:05 +0100

    gjort sök resultat betydligt snyggare

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import MovieGrid from "@/components/movieGrid";
-import VideoComponent from "@/components/videoComponent";
 
 export default function Home() {
   return (
     <>
       <div className="relative">
-        <VideoComponent />
         <MovieGrid />
       </div>
     </>
--- components/movieGrid.tsx
+++ components/movieGrid.tsx
@@ -11,6 +11,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "./ui/carousel";
+import VideoComponent from "./videoComponent";
 
 export default function MovieGrid() {
   const { trendingMovies, recommendedMovies } = useMovies();
@@ -18,149 +19,159 @@ export default function MovieGrid() {
 
   const isSearchEmpty = searchTerm.trim() === "";
   const isNoResults = searchResults.length === 0 && !isSearchEmpty;
+  const isSearching = !isSearchEmpty && searchResults.length > 0;
 
   return (
-    <div className="w-full px-14 z-20 ">
-      <div className="flex flex-col ">
-        {!isNoResults && (
-          <>
-            {!searchTerm && trendingMovies.length > 0 && (
-              <section className="flex flex-col gap-4 -translate-y-1/2 ">
-                <h2 className="title text-xl font-medium tracking-tight lg:text-2xl">
-                  Trendar just nu
-                </h2>
-                <Carousel
-                  opts={{
-                    align: "start",
-                    slidesToScroll: 1,
-                  }}
-                >
-                  <CarouselContent>
-                    {trendingMovies.map((movie, index) => (
-                      <CarouselItem
-                        key={index}
-                        className="sm:basis-1/2 md:basis-1/4 lg:basis-1/6"
-                      >
-                        <Card className="p-0 border-0 cursor-pointer ">
-                          <CardContent className="flex items-center justify-center p-0 h-56 ">
-                            <Link
-                              href={`/filmview/${movie.slug}`}
-                              as={`/filmview/${movie.slug}`}
-                            >
-                              <img
-                                className="object-cover w-full h-full "
-                                src={movie.thumbnail}
-                                alt={movie.title}
-                              />
-                            </Link>
-                          </CardContent>
-                          <div className="relative p-2 text-white bg-zinc-900 ">
-                            <FavoriteButton slug={movie.slug} />
-                          </div>
-                        </Card>
-                      </CarouselItem>
-                    ))}
-                  </CarouselContent>
+    <div>
+      {!isSearching && <VideoComponent />}
+      <div className={`${isSearching ? "px-14 z-20" : "w-full px-14 z-20"}`}>
+        <div
+          className={`${
+            isSearching
+              ? "flex w-full h-screen items-center justify-center "
+              : "flex flex-col"
+          }`}
+        >
+          {!isNoResults && (
+            <>
+              {!searchTerm && trendingMovies.length > 0 && (
+                <section className="flex flex-col gap-4 -translate-y-1/2 ">
+                  <h2 className="title text-xl font-medium tracking-tight lg:text-2xl">
+                    Trendar just nu
+                  </h2>
+                  <Carousel
+                    opts={{
+                      align: "start",
+                      slidesToScroll: 1,
+                    }}
+                  >
+                    <CarouselContent>
+                      {trendingMovies.map((movie, index) => (
+                        <CarouselItem
+                          key={index}
+                          className="sm:basis-1/2 md:basis-1/4 lg:basis-1/6"
+                        >
+                          <Card className="p-0 border-0 cursor-pointer ">
+                            <CardContent className="flex items-center justify-center p-0 h-56 ">
+                              <Link
+                                href={`/filmview/${movie.slug}`}
+                                as={`/filmview/${movie.slug}`}
+                              >
+                                <img
+                                  className="object-cover w-full h-full "
+                                  src={movie.thumbnail}
+                                  alt={movie.title}
+                                />
+                              </Link>
+                            </CardContent>
+                            <div className="relative p-2 text-white bg-zinc-900 ">
+                              <FavoriteButton slug={movie.slug} />
+                            </div>
+                          </Card>
+                        </CarouselItem>
+                      ))}
+                    </CarouselContent>
 
-                  <CarouselPrevious className="absolute top-1/2 left-2 transform -translate-y-1/2" />
-                  <CarouselNext className="absolute top-1/2 right-2 transform -translate-y-1/2" />
-                </Carousel>
-              </section>
-            )}
+                    <CarouselPrevious className="absolute top-1/2 left-2 transform -translate-y-1/2" />
+                    <CarouselNext className="absolute top-1/2 right-2 transform -translate-y-1/2" />
+                  </Carousel>
+                </section>
+              )}
 
-            {!searchTerm && recommendedMovies.length > 0 && (
-              <section className="flex flex-col gap-4 -translate-y-1/3">
-                <h2 className="title text-xl font-medium tracking-tight lg:text-2xl">
-                  Rekommenderat för dig
-                </h2>
-                <Carousel
-                  opts={{
-                    align: "start",
-                    slidesToScroll: 1,
-                  }}
-                >
-                  <CarouselContent>
-                    {recommendedMovies.map((movie, index) => (
-                      <CarouselItem
-                        key={index}
-                        className="sm:basis-1/2 md:basis-1/4 lg:basis-1/6"
-                      >
-                        <Card className="p-0 border-0 cursor-pointer ">
-                          <CardContent className="flex items-center justify-center p-0 h-56 overflow-hidden ">
-                            <Link
-                              href={`/filmview/${movie.slug}`}
-                              as={`/filmview/${movie.slug}`}
-                            >
-                              <img
-                                className="object-cover w-full h-full "
-                                src={movie.thumbnail}
-                                alt={movie.title}
-                              />
-                            </Link>
-                          </CardContent>
-                          <div className="relative p-2 text-white bg-zinc-900 ">
-                            <FavoriteButton slug={movie.slug} />
-                          </div>
-                        </Card>
-                      </CarouselItem>
-                    ))}
-                  </CarouselContent>
-                  <CarouselPrevious className="absolute top-1/2 left-2 transform -translate-y-1/2" />
-                  <CarouselNext className="absolute top-1/2 right-2 transform -translate-y-1/2" />
-                </Carousel>
-              </section>
-            )}
+              {!searchTerm && recommendedMovies.length > 0 && (
+                <section className="flex flex-col gap-4 -translate-y-1/3">
+                  <h2 className="title text-xl font-medium tracking-tight lg:text-2xl">
+                    Rekommenderat för dig
+                  </h2>
+                  <Carousel
+                    opts={{
+                      align: "start",
+                      slidesToScroll: 1,
+                    }}
+                  >
+                    <CarouselContent>
+                      {recommendedMovies.map((movie, index) => (
+                        <CarouselItem
+                          key={index}
+                          className="sm:basis-1/2 md:basis-1/4 lg:basis-1/6"
+                        >
+                          <Card className="p-0 border-0 cursor-pointer ">
+                            <CardContent className="flex items-center justify-center p-0 h-56 overflow-hidden ">
+                              <Link
+                                href={`/filmview/${movie.slug}`}
+                                as={`/filmview/${movie.slug}`}
+                              >
+                                <img
+                                  className="object-cover w-full h-full "
+                                  src={movie.thumbnail}
+                                  alt={movie.title}
+                                />
+                              </Link>
+                            </CardContent>
+                            <div className="relative p-2 text-white bg-zinc-900 ">
+                              <FavoriteButton slug={movie.slug} />
+                            </div>
+                          </Card>
+                        </CarouselItem>
+                      ))}
+                    </CarouselContent>
+                    <CarouselPrevious className="absolute top-1/2 left-2 transform -translate-y-1/2" />
+                    <CarouselNext className="absolute top-1/2 right-2 transform -translate-y-1/2" />
+                  </Carousel>
+                </section>
+              )}
 
-            {searchResults.length > 0 && (
-              <section className="flex flex-col gap-4 ">
-                <h2 className="title text-xl font-medium tracking-tight lg:text-2xl">
-                  Sökresultat
-                </h2>
-                <Carousel
-                  opts={{
-                    align: "start",
-                    slidesToScroll: 1,
-                  }}
-                >
-                  <CarouselContent>
-                    {searchResults.map((movie, index) => (
-                      <CarouselItem
-                        key={index}
-                        className="sm:basis-1/2 md:basis-1/4 lg:basis-1/4"
-                      >
-                        <Card className="p-0 border-0 cursor-pointer">
-                          <CardContent className="flex items-center justify-center p-0 h-64">
-                            <Link
-                              href={`/filmview/${movie.slug}`}
-                              as={`/filmview/${movie.slug}`}
-                            >
-                              <img
-                                className="object-cover w-full h-full  "
-                                src={movie.thumbnail}
-                                alt={movie.title}
-                              />
-                            </Link>
-                          </CardContent>
-                          <div className="relative p-2 text-white bg-zinc-900 ">
-                            <FavoriteButton slug={movie.slug} />
-                          </div>
-                        </Card>
-                      </CarouselItem>
-                    ))}
-                  </CarouselContent>
-                  <CarouselPrevious className="absolute top-1/2 left-2 transform -translate-y-1/2" />
-                  <CarouselNext className="absolute top-1/2 right-2 transform -translate-y-1/2" />
-                </Carousel>
-              </section>
-            )}
-          </>
-        )}
+              {searchResults.length > 0 && (
+                <section className="flex flex-col gap-4 ">
+                  <h2 className="title text-xl font-medium tracking-tight lg:text-2xl">
+                    Sökresultat
+                  </h2>
+                  <Carousel
+                    opts={{
+                      align: "start",
+                      slidesToScroll: 1,
+                    }}
+                  >
+                    <CarouselContent>
+                      {searchResults.map((movie, index) => (
+                        <CarouselItem
+                          key={index}
+                          className="sm:basis-1/2 md:basis-1/4 lg:basis-1/4"
+                        >
+                          <Card className="p-0 border-0 cursor-pointer">
+                            <CardContent className="flex items-center justify-center p-0 h-64">
+                              <Link
+                                href={`/filmview/${movie.slug}`}
+                                as={`/filmview/${movie.slug}`}
+                              >
+                                <img
+                                  className="object-cover w-full h-full  "
+                                  src={movie.thumbnail}
+                                  alt={movie.title}
+                                />
+                              </Link>
+                            </CardContent>
+                            <div className="relative p-2 text-white bg-zinc-900 ">
+                              <FavoriteButton slug={movie.slug} />
+                            </div>
+                          </Card>
+                        </CarouselItem>
+                      ))}
+                    </CarouselContent>
+                    <CarouselPrevious className="absolute top-1/2 left-2 transform -translate-y-1/2" />
+                    <CarouselNext className="absolute top-1/2 right-2 transform -translate-y-1/2" />
+                  </Carousel>
+                </section>
+              )}
+            </>
+          )}
 
-        {isNoResults && (
-          <p className="text-xl text-center font-bold tracking-tight lg:text-2xl">
-            Det finns inga sökresultat.
-          </p>
-        )}
+          {isNoResults && (
+            <p className="text-xl text-center font-bold tracking-tight lg:text-2xl">
+              Det finns inga sökresultat.
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
