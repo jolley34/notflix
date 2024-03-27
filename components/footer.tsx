af9d5f08297a188bf93a92caf3cf019d07bd12d9
jolley34 <erlandssonjoel@gmail.com>
2024-03-27 10:38:35 +0100

    bytt ut en bild och fixat spacing i footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-secondary-foreground p-20 w-full bottom-0 ">
-      <div className="flex justify-between  items-center">
+    <footer className="bg-secondary-foreground p-16 w-full bottom-0 ">
+      <div className="flex justify-between items-center">
         <div className="flex gap-2 items-center">
           <h1>Notflix ™</h1>
           <h1 className="font-thin text-xs ">1997-2024</h1>
