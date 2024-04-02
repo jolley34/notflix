098cccc427214917339060ce9dbf1e0c0fd53b35
jolley34 <erlandssonjoel@gmail.com>
2024-04-02 09:50:01 +0200

    fixat ett strangerthings bildspel

--- components/randomBackground.tsx
+++ components/randomBackground.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const randomFrames = [
   {
@@ -21,46 +21,23 @@ const randomFrames = [
     logo: "/StrangerThings_logo.svg",
     trailerSrc: "/stranger-things-trailer.mp4",
   },
-  {
-    series: "Ozark",
-    images: [
-      "/image-1-oz.png",
-      "/image-2-oz.png",
-      "/image-3-oz.png",
-      "/image-4-oz.png",
-      "/image-5-oz.png",
-      "/image-6-oz.png",
-      "/image-7-oz.png",
-      "/image-8-oz.png",
-      "/image-9-oz.png",
-      "/image-10-oz.png",
-    ],
-    logo: "/ozark-logo.svg",
-    trailerSrc: "/ozark-trailer.mp4",
-  },
 ];
 
 export default function RandomBackground() {
-  const [currentFrameIndex, setCurrentFrameIndex] = useState(
-    Math.floor(Math.random() * randomFrames.length)
-  );
-  const [currentImageIndex, setCurrentImageIndex] = useState(
-    Math.floor(Math.random() * 10)
-  );
   const [playingTrailer, setPlayingTrailer] = useState(false);
+  const [currentFrame] = useState(randomFrames[0]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const imageRef = useRef(null);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (!playingTrailer) {
-        setCurrentImageIndex(Math.floor(Math.random() * 10));
-        setCurrentFrameIndex(Math.floor(Math.random() * randomFrames.length));
-      }
-    }, 20000);
-    return () => clearInterval(interval);
-  }, [playingTrailer]);
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === currentFrame.images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 2000);
 
-  const currentFrame = randomFrames[currentFrameIndex];
-  const currentLogo = currentFrame.logo;
+    return () => clearInterval(interval);
+  }, [currentFrame.images.length]);
 
   const playTrailer = () => {
     setPlayingTrailer(true);
@@ -80,33 +57,31 @@ export default function RandomBackground() {
           onEnded={endTrailer}
         />
       ) : (
-        randomFrames.map((frame, index) => (
-          <img
-            key={index}
-            src={frame.images[currentImageIndex]}
-            alt="Random Frame"
-            className="absolute top-0 left-0 w-screen h-screen object-cover transition-opacity duration-1000"
-            style={{
-              opacity: index === currentFrameIndex ? 1 : 0,
-            }}
-          />
-        ))
+        <>
+          {currentFrame.images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt="Random Frame"
+              className="absolute w-screen h-screen object-cover"
+              style={{
+                opacity: index === currentImageIndex ? 1 : 0,
+                transition: "opacity 1s ease",
+              }}
+            />
+          ))}
+        </>
       )}
       <div className="absolute px-16 flex justify-center items-center top-1/2 -translate-y-1/2">
         <div className="flex flex-col gap-4 items-left">
           <img
-            key={currentLogo}
-            src={currentLogo}
+            key={currentFrame.logo}
+            src={currentFrame.logo}
             alt="Random Logo"
-            className="transition-opacity duration-1000"
             style={{
               width: "400px",
               height: "auto",
               opacity: 0.7,
-              filter:
-                currentFrame.series === "Ozark"
-                  ? "brightness(0) invert(1)"
-                  : "none",
             }}
           />
           {!playingTrailer && (
@@ -119,12 +94,11 @@ export default function RandomBackground() {
           </Button>
         </div>
       </div>
-
       <div
         className="absolute inset-x-0 bottom-0 h-52"
         style={{
           background:
-            "linear-gradient(180deg, transparent 0 5%, hsl(var(--background)) 100% 100%)",
+            "linear-gradient(180deg, transparent 0%, hsl(var(--background)) 100%)",
         }}
       ></div>
     </div>
